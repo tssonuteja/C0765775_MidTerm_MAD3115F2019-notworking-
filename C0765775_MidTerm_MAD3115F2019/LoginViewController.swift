c2eafda85a3ec9d4a3e1357d667fb66fccfb2013
saiteja <saitejasunkari04@gmail.com>
2019-11-01 13:09:35 -0400

    login connected 10

--- C0765775_MidTerm_MAD3115F2019/LoginViewController.swift
+++ C0765775_MidTerm_MAD3115F2019/LoginViewController.swift
@@ -21,6 +21,12 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func Login(_ sender: UIButton) {
+    }
+    
+    
+    
 
 }
 
